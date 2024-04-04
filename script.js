78a2036cd36cbc10d48bb26a69029ad8186eed95
Rajat <rajatsaxena6397@gmail.com>
2024-04-04 14:20:02 +0530

    update

--- script.js
+++ script.js
@@ -64,26 +64,84 @@ function wikipedia() {
 }
 
 //   Google //
-function search() {
-  const input = document.querySelector("#searchInput");
-  const resultsContainer = document.querySelector(".results");
-  const searchTerm = input.value;
-  if (searchTerm) {
-    searchGoogle(searchTerm);
-  }
-  // const input = searchInput.value;
+// function search() {
+//   const input = document.querySelector("#searchInput");
+//   const resultsContainer = document.querySelector(".results");
+//   const searchTerm = input.value;
+//   if (searchTerm) {
+//     searchGoogle(searchTerm);
+//   }
+//   // const input = searchInput.value;
 
-  function searchGoogle(searchTerm) {
-    let url = 'https://www.google.com/search?q=' +searchInput.value;
-    window.open(url);
-
+//   function searchGoogle(searchTerm) {
+//     let url = 'https://www.google.com/search?q=' +searchInput.value;
+//     window.open(url);
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
     // window.location.href =
     //   "https://www.google.com/search?q=" +
     //   input +
     //   "&rlz=1C5CHFA_enNZ948NZ948&oq=" +
     //   input +
     //   "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8";
-  }
+//   }
+// }
+let arr =[];
+ function search() {
+    const input = document.querySelector("#searchInput");
+  const url = `https://google-search74.p.rapidapi.com/?query=${input.value}&limit=10&related_keywords=true`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0b098ddd3bmshc2088717c58397cp1875a3jsndd8384e6ce4b',
+		'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+	}
+};
+searchGoogle();
+async function searchGoogle(){
+    
+    try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	// console.log(result);
+    arr.push(result.results);
+     console.log(arr.length);
+    display();
+
+} catch (error) {
+	console.error(error);
 }
+}
+ }
+
+function display(){
+    let googledata = document.querySelector(".results");
+    googledata.innerHTML="";
+     arr[0].forEach((ele, index)=>{
+     console.log(ele);
+        // for(let i=0; i<arr.length; i++)
+        // {
+        // if(i==1)
+        // {
+        // console.log(arr);
+        // }
+    
+let div = document.createElement("div");
+ div.innerHTML = `<h2 class="change">${ele.title}</h2> <p>${ele.description}</p> <a href=${ele.url}>${ele.url}</a>`;
+ googledata.appendChild(div);
+ 
+    // }
+    
+
+         });
+     }
+
+
+ 
 
 // Gemini //
